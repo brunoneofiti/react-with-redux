5f1d5e7b03cb6eb2cf4da76ae34000331ca0105d
Bruno Neofiti De Andrade <bneofiti@bbmapfre.com.br>
2019-11-14 11:42:00 -0300

    final da aplicacao, ainda sem redux

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -9,32 +9,75 @@ const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = { description: '', list: [] }
+
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPengind = this.handleMarkAsPengind.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
+        this.refresh()
     }
 
-    handleChange(e){
-        this.setState({...this.state, description: e.target.value})
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({ ...this.state, description, list: resp.data }))
     }
 
-    handleAdd(){
+    handleClear() {
+        this.refresh()
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou!!'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh)
+    }
+
+    handleMarkAsPengind(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh())
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm 
+                <TodoForm
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
-                    description={this.state.description}  />
-                <TodoList />
+                    description={this.state.description}
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear} />
+                <TodoList
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPengind={this.handleMarkAsPengind} />
             </div>
         )
     }
